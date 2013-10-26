30568fb36f86d435e3ef658836aff254ff1f4625
bhunt2 <bhunt2@pdx.edu>
2013-10-25 21:43:50 -0700

    Did some more moving around and adding some code and libraries from Atmel that might be useful.

---
+++ Code/LCD/ProjectTrialLCD1/ProjectTrialLCD1/ProjectTrialLCD1.c
@@ -0,0 +1,17 @@
+/*
+ * ProjectTrialLCD1.c
+ *
+ * Created: 10/25/2013 9:38:31 PM
+ *  Author: Ben
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
