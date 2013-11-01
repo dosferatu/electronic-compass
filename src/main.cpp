07a9b6c1d440942bd2d2c27816bda5c4d5c0407d
Michael <waltonm@pdx.edu>
2013-10-31 20:01:35 -0700

    Start integrating Atmel supplied TWI driver in to project.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,32 @@
 /*
- *
+ * This is the routine for the electronic compass.
+ * The ATMega32U4 will be initialized making sure to
+ * unmask interrupts for the I2C bus and the necessary
+ * GPIO interrupts for the magnetometer/accelerometer
+ * and LCD display respectively. From here we simply
+ * take readings from the sensor package, and map the
+ * results to display values for the LCD.
  */
 
-int main(int argc, char **argv)
+// Register definitions.
+//#define INT_TWI 0x0030	// 2-wire Serial Interface Interrupt
+
+#include "TWI_Master.h"
+
+//11100101
+//unsigned char twcr = 0xE5;
+
+int main()
 {
-	//
+		TWI_Master_Initialise();
+
+		//while(true)
+		//{
+				// Tell sensor we want a reading
+				// wait for interrupt from sensor
+				// read in sensor data
+				// calculate heading
+				// display to lcd
+		//}
+		return 0;
 }
