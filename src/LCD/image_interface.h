55953d759bdf58c043a7c013d12e2075406d6d58
bhunt2 <bhunt2@pdx.edu>
2013-11-18 11:09:30 -0800

    Loads some updates to the LCD display and images

--- src/LCD/image_interface.h
+++ src/LCD/image_interface.h
@@ -5,7 +5,35 @@
     is where the bytes are passed for the left column from top to bottom and then moves to the next column to the right,
     horizontal is where the bytes are passed for the top column from left to right and then moves to the next row down.
 
-    The structure of the LCD has been chosen to be a linear array of chars with each index representing one of the
+    The structure of the LCD has been chosen to be a two dimensional array of chars with each index representing one of the
     bytes passed to the PCD8544.  There will be an array that will be used for storing the currently processing image and
     there will be an array that will be used for making necessary updates to the image, a working image as it were.  The
-    working array can be
+    working array can be updated as quickly as the data is updated from the mag/accel.  This will then be copied to the
+    processing image for processing.
+*/
+
+// Image processing class
+class image_processing
+{
+    public:
+        // Functions for editing the image
+        load_proc_img(unsigned char [][] new_img);          // Pass it the desired base image of the appropriate size in hex format
+        edit_working_img(unsigned char * new_part, int byte_x, int byte_y);         // Pass it a new part of
+
+
+        // Letters & Numbers
+        const unsigned char Letters [][7] = {
+        {0xFF, 0x08, 0x30, 0x30, 0xC0, 0x00, 0xFF},
+        {0x07, 0x00, 0x00, 0x00, 0x00, 0x03, 0x07},
+        {0x0E, 0x11, 0x11, 0x11, 0x11, 0x11, 0xE0},
+        {0x00, 0x04, 0x04, 0x04, 0x04, 0x04, 0x03},
+        {0xFF, 0x41, 0x41, 0x41, 0x41, 0x41, 0x01},
+        {0x07, 0x04, 0x04, 0x04, 0x04, 0x04, 0x04},
+        {0x7F, 0xC0, 0x80, 0xF0, 0x80, 0xC0, 0x7F},
+        {0x00, 0x01, 0x07, 0x00, 0x07, 0x01, 0x00}};
+
+    private:
+        // Arrays
+        unsigned char proc_img [6][84];              // Array for holding the currently processing image
+        unsigned char working_img [6][84];           // Array for holding the working image
+};
